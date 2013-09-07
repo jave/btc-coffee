1b6027387eaa572216b2eee06464e08ec7ebcb90
Joakim Verona <joakim@verona.se>
2013-09-08 00:15:57 +0200

    added example configuration file

---
+++ btc-coffee.clj
@@ -0,0 +1,11 @@
+;;edit and rename to ~/.btc-coffee.clj 
+{
+ :ocx-key "you must get your own app key"
+ :bitcoind-user "bitcoinrpc" 
+ :bitcoind-password "the pwd to your bitcoind"
+ :account "your bitcoind account"
+ :label "coffee"
+ :message "coffee"
+ :amount 1
+ :from :SEK
+ }
